d82bb6b1be5e432f8878c3ca58572799969e5f0e
Thomas Spiesser <thomas.spiesser@gmail.com>
2014-09-16 17:00:18 +0200

    adds failing logIn

--- toptaskClient/toptaskClient/AppDelegate.swift
+++ toptaskClient/toptaskClient/AppDelegate.swift
@@ -17,14 +17,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var meteorClient = initialiseMeteor("pre2", "http://localhost:3000/websocket");
 
 
-
-
-
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
         // Override point for customization after application launch.
+        meteorClient.logonWithEmail("m@g.com", password: "111111", responseCallback: {(response, error) -> Void in if((error) != nil) {
+                println(error)
+                self.handleFailedAuth(error)
+                return
+            }
+            self.handleSuccessfulAuth()
+        })
+
         return true
     }
 
+    func handleSuccessfulAuth() {
+        println("success")
+    }
+    
+    func handleFailedAuth(error: NSError) {
+        UIAlertView(title: "Meteor Todos", message:error.localizedDescription, delegate: nil, cancelButtonTitle: "Try Again").show()
+    }
+    
     func applicationWillResignActive(application: UIApplication!) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
