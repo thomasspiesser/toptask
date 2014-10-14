f744af384ad2311a53c3903a9fe9e57eaf09aff7
Thomas Spiesser <thomas.spiesser@gmail.com>
2014-10-14 22:39:43 +0200

    adds tableViewController and taskItem class

---
+++ toptaskClient/toptaskClient/TaskItem.swift
@@ -0,0 +1,19 @@
+//
+//  TaskItem.swift
+//  toptaskClient
+//
+//  Created by Thomas Spiesser on 14.10.14.
+//  Copyright (c) 2014 Thomas Spiesser. All rights reserved.
+//
+
+import UIKit
+
+class TaskItem: NSObject {
+    var taskName: NSString = ""
+    var checked: Bool = false
+    var creationDate: NSDate = NSDate()
+    
+    init(name:String){
+        self.taskName = name
+    }
+}
